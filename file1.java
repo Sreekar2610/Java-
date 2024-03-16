90f22a94485bd6f8dfb44ff2c44f9944940ea085
sreekar nyalati <149220369+Sreekar2610@users.noreply.github.com>
2024-03-16 15:32:34 +0530

    Add files via upload

---
+++ file1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class file1
+{
+	public static void main(String[] ar)
+	{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter your name");
+	String name=sc.nextLine();
+	System.out.println("welcome"+name);
+}
+}
+
+         
---
+++ file2.java
@@ -0,0 +1,6 @@
+//command line arguments.
+class file2{
+	public static void main(String[] p){
+		System.out.println("welcome"+p[0]);
+	}
+}
