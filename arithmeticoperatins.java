487a91663a0bbe7c2033f22d82c640790f2e1347
sreekar nyalati <149220369+Sreekar2610@users.noreply.github.com>
2024-03-21 13:00:52 +0530

    Add files via upload

---
+++ arithmeticoperatins.java
@@ -0,0 +1,13 @@
+class arithmeticoperations
+	{
+      	public static void main(String[] args)
+	 {
+ 	  int a=5,b=6;
+	System.out.println("sum is:"+(a+b));
+          System.out.println("difference is:"+(a-b));
+	  System.out.println("product is :"+(a*b));
+	  System.out.println("quotient is :"+(a/b));
+	  System.out.println("remainder is :"+(a%b));
+ 	  }
+}	
+   
