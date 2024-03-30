c93409b93cb14f93b167c0dc8b1dde9619dc0777
sreekar nyalati <149220369+Sreekar2610@users.noreply.github.com>
2024-03-30 14:35:19 +0530

    Add files via upload

---
+++ 30-3-24/conct.java
@@ -0,0 +1,12 @@
+class conct{
+	public static void main(String[] args){
+	   String first="java";
+	   String second="lab";
+	   String cmp=first.concat(second);
+	   System.out.println(cmp);
+	   String name="james";
+	   String lname="gosling";
+	   String auth=name+lname;
+	   System.out.println(auth);
+  } 
+}
---
+++ 30-3-24/str.java
@@ -0,0 +1,5 @@
+class str{
+	public static void main(String[] args){
+	 String str1="helloo";
+
+	 
