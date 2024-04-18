dd0c67cbb556c005ba09d53b7b2e34aa75cd3d95
sreekar nyalati <149220369+Sreekar2610@users.noreply.github.com>
2024-04-18 15:41:17 +0530

    Add files via upload

---
+++ strbuffer.java
@@ -0,0 +1,8 @@
+public class strbuffer{
+	public static void main(String[] args){
+	  StringBuffer sb=new StringBuffer("hello");
+	  System.out.println(sb);
+	  sb.append("java");
+	  System.out.println(sb);
+}
+}
